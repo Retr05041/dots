23d4137b1c16b18fbb755e1dd7d788e1d9185b5e
Retr05041 <parker@cranfield.ca>
2024-04-24 11:50:29 -0700

    Begun work on Arch stuff

---
+++ Arch/scripts/install_base_deb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pacman -S --needed base-devel git sudo nitrogen terminator dmenu firefox i3-wm lightdm lightdm-slick-greeter networkmanager vim xorg-server keepassxc i3status
---
+++ Arch/scripts/install_nvim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yay -S neovim-git
