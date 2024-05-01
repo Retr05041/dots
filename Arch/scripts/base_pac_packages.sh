3472ed52f00cc8ffc762794caff4df25c0a1ce2f
Retr05041 <parker@cranfield.ca>
2024-05-01 07:59:22 -0700

    Fix font issue.. for now

--- Arch/scripts/base_pac_packages.sh
+++ Arch/scripts/base_pac_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Needed
-pacman -S --needed base-devel feh wezterm dmenu i3-wm lightdm lightdm-slick-greeter i3blocks networkmanager vim xorg-server alsa-utils picom acpi noto-fonts-emoji 
+pacman -S --needed base-devel feh wezterm dmenu i3-wm lightdm lightdm-slick-greeter i3blocks networkmanager vim xorg-server alsa-utils picom acpi noto-fonts-emoji noto-fonts-extra
 
 # Optional
 pacman -S --needed firefox keepassxc zoxide discord
