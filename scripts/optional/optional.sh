08e9236ca12aa80a504a922bb9feb6528dcd99b8
Retr05041 <parker@cranfield.ca>
2024-08-24 15:20:30 -0700

    screen shotting and remove tmux line

--- scripts/optional/optional.sh
+++ scripts/optional/optional.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Optional
-pacman -S --noconfirm --needed firefox keepassxc zoxide discord glow nodejs-lts-iron npm ripgrep htop
+pacman -S --noconfirm --needed firefox keepassxc zoxide discord glow nodejs-lts-iron npm ripgrep htop flameshot
 
