fd4e39257e05566c21c295df5ffc06e933e6ac71
Retr05041 <parker@cranfield.ca>
2024-12-08 20:10:34 +0000

    Made terminal darker, thinking about revamping this

--- configs/core/wezterm/wezterm.lua
+++ configs/core/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.colors = { background = 'black' }
 config.color_scheme = 'Tokyo Night'
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.font = wezterm.font 'Hack Nerd Font'
 
 return config
